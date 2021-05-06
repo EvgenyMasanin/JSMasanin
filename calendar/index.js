b4afee62c5304a45c2b315c33d81d6f0792a2c74
MasaninE <gmasmin@gmail.com>
2021-05-06 20:12:48 +0300

    add config

--- calendar/index.js
+++ calendar/index.js
@@ -9,13 +9,23 @@ tableContainer.style.color = '#e9e9e9'
 tableContainer.style.width = '390px'
 tableContainer.style.padding = '10px'
 wrapper.append(tableContainer)
-const cash = new Set()
+const holidays = {
+    '9 мая': 'orange',
+    '1 мая': 'red',
+    '8 марта': '#ff869a',
+    '23 февраля': 'orange',
+    '25 июля': 'blue'
+}
+const importantDates = new Set()
 let now = new Date()
-createCalendar(tableContainer, table, now, 2)
+createCalendar(tableContainer, table, now, {
+    firstDayNum: 2,
+    holidays
+})
 
 
 
-function createCalendar(tableContainer, table, now, firstDayNum) {
+function createCalendar(tableContainer, table, now, config) {
     createHeader()
     createBody(now)
 
@@ -27,7 +37,10 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
         )
         firstDay.setDate(1)
 
-        let dayNum = (firstDay.getDay() - firstDayNum) * -1
+        let dayNum
+        firstDay.getDay() === 0 ?
+            dayNum = (7 - config.firstDayNum) * -1 :
+            dayNum = (firstDay.getDay() - config.firstDayNum) * -1
 
         for (let i = 0; i < 6; i++) {
             const tr = document.createElement('tr')
@@ -50,7 +63,7 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
                 if (tmp.getMonth() !== now.getMonth()) {
                     th.style.color = 'gray'
                 }
-                if (cash.has(tmpDate)) {
+                if (importantDates.has(tmpDate)) {
                     th.style.color = 'red'
                 }
                 const rightNow = new Date()
@@ -58,20 +71,26 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
                     th.style.backgroundColor = '#0078d7'
                     th.style.outline = '3px #0078d7 solid'
                 }
+                const holiday = tmp.toLocaleString("ru", {
+                    month: 'long',
+                    day: 'numeric'
+                })
+                if(holiday in config.holidays) {
+                    th.style.borderColor = config.holidays[holiday]
+                }
                 th.append(tmp.getDate())
                 tr.append(th)
                 dayNum++
                 th.addEventListener('click', () => {
                     if (th.style.color === 'red') {
                         tmp.getMonth() !== now.getMonth() ?
-                        th.style.color = 'gray' :   
-                        th.style.color = 'white'
-
-                        cash.delete(tmpDate)
+                            th.style.color = 'gray' :
+                            th.style.color = 'white'
+                        importantDates.delete(tmpDate)
                     }
                     else {
                         th.style.color = 'red'
-                        cash.add(tmpDate)
+                        importantDates.add(tmpDate)
                     }
                 })
             }
@@ -89,6 +108,20 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
             'Пт',
             'Сб',
             'Вс',
+        ]
+        const months = [
+            'Январь',
+            'Февраль',
+            'Март',
+            'Апрель',
+            'Май',
+            'Июнь',
+            'Июль',
+            'Август',
+            'Сентябрь',
+            'Октябрь',
+            'Ноябрь',
+            'Декабрь'
         ]
 
         const firstHeader = document.createElement('div')
@@ -121,15 +154,15 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
         rightButton.innerHTML = '<i class="fas fa-chevron-down"></i>'
 
         changeButton.addEventListener('click', () => {
-            firstDayNum === 2 ? firstDayNum = 1 : firstDayNum = 2
-            refresh(0, firstDayNum)
+            config.firstDayNum === 2 ? config.firstDayNum = 1 : config.firstDayNum = 2
+            refresh(0, config.firstDayNum)
         })
 
         leftButton.addEventListener('click', () => {
-            refresh(-1, firstDayNum)
+            refresh(-1, config.firstDayNum)
         })
         rightButton.addEventListener('click', () => {
-            refresh(1, firstDayNum)
+            refresh(1, config.firstDayNum)
         })
 
         function refresh(direction, firstDayNum) {
@@ -137,14 +170,99 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
             firstHeader.innerHTML = ''
             tableContainer.innerHTML = ''
             now.setMonth(now.getMonth() + direction)
-            createCalendar(tableContainer, table, now, firstDayNum)
+            createCalendar(tableContainer, table, now, config)
+        }
+        {
+        // const dataSelect = document.createElement('input')
+        // dataSelect.setAttribute('type', 'month')
+        // dataSelect.setAttribute('min', '1000-01')
+        // dataSelect.style.width = '170px'
+        // dataSelect.style.color = 'white'
+        // dataSelect.style.fontWeight = 'bold'
+        // dataSelect.style.backgroundColor = 'inherit'
+        // dataSelect.style.border = 'none'
+        // dataSelect.style.fontSize = '16px'
+        // if (now.getMonth() < 9) {
+        //     dataSelect.value = now.getFullYear() + '-0' + (now.getMonth() + 1)
+        // }
+        // else {
+        //     dataSelect.value = now.getFullYear() + '-' + (now.getMonth() + 1)
+        // }
+        // dataSelect.addEventListener('change', () => {
+        //     let data = dataSelect.value.split('-')
+        //     const newDate = new Date(
+        //         data[0],
+        //         data[1] - 1
+        //     )
+        //     console.log(data);
+        //     table.innerHTML = ''
+        //     firstHeader.innerHTML = ''
+        //     tableContainer.innerHTML = ''
+        //     createCalendar(tableContainer, table, newDate, firstDayNum, holidays)
+        // })
+        }
+
+        const dataContainer = document.createElement('div')
+        dataContainer.style.display = 'flex'
+        dataContainer.style.width = '160px'
+        dataContainer.style.justifyContent = 'space-between'
+        
+
+        const dataSelect = document.createElement('select')
+        dataSelect.style.color = 'white'
+        dataSelect.style.fontWeight = 'bold'
+        dataSelect.style.backgroundColor = 'inherit'
+        dataSelect.style.border = 'none'
+        dataSelect.style.borderRadius = '5px'
+        dataSelect.style.fontSize = '16px'
+        months.forEach((month, num) => {
+            let option = document.createElement('option')
+            option.setAttribute('value', num)
+            option.textContent = month
+            option.style.backgroundColor = '#26262a'
+            option.style.border = 'none'
+            dataSelect.options.add(option)
+        });
+
+        dataSelect.value = now.getMonth()
+        const yearInput = document.createElement('input')
+        yearInput.value = now.getFullYear()
+        yearInput.style.width = '50px'
+        yearInput.style.color = 'white'
+        yearInput.style.fontWeight = 'bold'
+        yearInput.style.backgroundColor = 'inherit'
+        yearInput.style.border = 'none'
+        yearInput.style.borderBottom = '1px Solid #c2bfbf'
+
+        yearInput.addEventListener('change', changeDate);
+        dataSelect.addEventListener('change', changeDate);
+        function changeDate() {
+            const string = yearInput.value.match('[0-9]{4}')
+            if(string && string[0] == string.input) {
+                const newDate = new Date(
+                    yearInput.value,
+                    dataSelect.value
+                )
+                dataSelect.selectedOptions = dataSelect.value
+                table.innerHTML = ''
+                firstHeader.innerHTML = ''
+                tableContainer.innerHTML = ''
+                createCalendar(tableContainer, table, newDate, config)
+            }
+            else {
+                yearInput.value = now.getFullYear()
+            }
+            
         }
 
         buttonContainer.append(changeButton)
         buttonContainer.append(leftButton)
         buttonContainer.append(rightButton)
 
-        firstHeader.append(title)
+        dataContainer.append(dataSelect)
+        dataContainer.append(yearInput)
+
+        firstHeader.append(dataContainer)
         firstHeader.append(buttonContainer)
         tableContainer.append(firstHeader)
 
@@ -155,13 +273,16 @@ function createCalendar(tableContainer, table, now, firstDayNum) {
             td.style.padding = '3px'
             td.style.textAlign = 'center'
             td.style.fontWeight = 'bold'
-            if (firstDayNum === 1) {
+            if (config.firstDayNum === 1) {
                 ind === 0 ? td.textContent = days[6] : td.textContent = days[ind - 1]
                 secondHeader.append(td)
             }
             else {
                 td.textContent = day
                 secondHeader.append(td)
+            }
+            if (td.textContent === days[5] || td.textContent === days[6]) {
+                td.style.color = 'red'
             }
         })
         table.append(secondHeader)
